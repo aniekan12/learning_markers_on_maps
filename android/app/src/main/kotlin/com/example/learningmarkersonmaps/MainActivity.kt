901bdddc290d2ddd2ded634321b0d54128ea7ba8
greenlite <greenlite@greenlites-MacBook-Pro.local>
2022-02-10 17:44:41 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/learningmarkersonmaps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learningmarkersonmaps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/display_user_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DisplayUserView extends StatefulWidget {
+  const DisplayUserView({Key? key}) : super(key: key);
+
+  @override
+  _DisplayUserViewState createState() => _DisplayUserViewState();
+}
+
+class _DisplayUserViewState extends State<DisplayUserView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
